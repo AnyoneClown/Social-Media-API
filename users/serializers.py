37b7f133619af09640722e57a09de40e5137b2b4
Denis Dymerlii <dum.denya15@gmail.com>
2024-01-16 13:12:30 +0200

    create view for register, login and logout. Serializer for user model

---
+++ users/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "email", "password", "is_staff")
+        read_only_fields = ("id", "is_staff")
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
