a2d47277069c8a02342b2a867c6b1c496f970c23
Denis Dymerlii <dum.denya15@gmail.com>
2024-01-16 13:12:11 +0200

    update AUTH_USER_MODEL in settings.py

--- Social_Media_API/settings.py
+++ Social_Media_API/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "social_media",
     "users",
 ]
@@ -124,3 +125,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
