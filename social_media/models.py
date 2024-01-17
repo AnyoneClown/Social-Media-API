c9ddac7581ebad9fdcd788989bb3d4c1f04159e7
Denis Dymerlii <dum.denya15@gmail.com>
2024-01-18 00:24:59 +0200

    update migration for models, add commentary and like models

--- social_media/models.py
+++ social_media/models.py
@@ -30,6 +30,25 @@ class Profile(models.Model):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(null=True, upload_to=profile_image_file_path)
     created_at = models.DateTimeField(auto_now_add=True)
+    followers = models.ManyToManyField("Follow", related_name="profiles")
+    posts = models.ManyToManyField("Post", related_name="profiles")
+
+    def __str__(self) -> str:
+        return self.user.email
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="follows",
+    )
+    following = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="follows",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Post(models.Model):
@@ -40,3 +59,35 @@ class Post(models.Model):
     text = models.TextField()
     title = models.CharField(max_length=255)
     image = models.ImageField(null=True, upload_to=post_image_file_path)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField("Like", related_name="post_likes")
+    commentaries = models.ManyToManyField("Commentary", related_name="post_commentaries")
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="like_users",
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="post_likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="commentary_users",
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="post_commentary"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
